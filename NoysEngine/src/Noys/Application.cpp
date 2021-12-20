b59d6150da30e49722e92089fe12e071f7d21c17
Jose Ramon Arias <joseramon.arias28@gmail.com>
2021-12-19 18:25:48 -0600

    Added Event System

--- NoysEngine/src/Noys/Application.cpp
+++ NoysEngine/src/Noys/Application.cpp
@@ -1,4 +1,6 @@
 #include "Application.h"
+#include "Noys/Events/ApplicationEvent.h"
+#include "Noys/Log.h"
 
 namespace Noys
 {
@@ -14,6 +16,9 @@ namespace Noys
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		NY_TRACE(e);
+
 		while (true);
 	}
 }
--- premake5.lua
+++ premake5.lua
@@ -26,6 +26,7 @@ project "NoysEngine"
 
 	includedirs
 	{
+		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include"
 	}
 
